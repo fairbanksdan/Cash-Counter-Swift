8506ff7613a78078e37440e55990fc697e75a4cd
Dan Fairbanks <fairbanksdan@gmail.com>
2014-06-05 14:13:00 -0700

    first Commit

--- Cash Counter Swift/ViewController.swift
+++ Cash Counter Swift/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    var str: Int = 0
+    var pennyCount = 0.00
+    let pennymultiplier = 0.01
+    
+    @IBOutlet var resetButton : UIButton
+    @IBOutlet var grandTotalLabel : UILabel
+    @IBOutlet var pennyTotalLabel : UILabel
+    
+    
+    @IBOutlet var pennyTextField : UITextField
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +29,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func pennyCounter(sender : UITextField) {
+        str = pennyTextField.text.toInt()!
+        
+        pennyCount = Float(str) * 0.01
+        
+        self.UpdateLabels()
+    }
+    
+    func UpdateLabels() {
+        pennyTotalLabel.text = String(str)
+        
+    }
 
-
+    @IBAction func ResetButtonPressed(sender : UIButton) {
+    }
+    
+    
 }
 
