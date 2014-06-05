d236ce14806b94701017a276740421332b11f59a
Dan Fairbanks <fairbanksdan@gmail.com>
2014-06-05 15:24:46 -0700

    fixed issue with overload error

--- Cash Counter Swift/ViewController.swift
+++ Cash Counter Swift/ViewController.swift
@@ -10,19 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
     var str: Int = 0
-    var pennyCount = 0.00
+    var pennyCount: Double = 0.00
     let pennymultiplier = 0.01
     
     @IBOutlet var resetButton : UIButton
     @IBOutlet var grandTotalLabel : UILabel
     @IBOutlet var pennyTotalLabel : UILabel
-    
+    @IBOutlet var nickelTotalLabel : UILabel
+    @IBOutlet var dimeTotalLabel : UILabel
+    @IBOutlet var quarterTotalLabel : UILabel
+    @IBOutlet var fiftyCentTotalLabel : UILabel
+    @IBOutlet var singleDollarTotalLabel : UILabel
+    @IBOutlet var twoDollarTotalLabel : UILabel
+    @IBOutlet var fiveDollarTotalLabel : UILabel
+    @IBOutlet var tenDollarTotalLabel : UILabel
+    @IBOutlet var twentyDollarTotalLabel : UILabel
+    @IBOutlet var fiftyDollarTotalLabel : UILabel
+    @IBOutlet var hundredDollarTotalLabel : UILabel
     
     @IBOutlet var pennyTextField : UITextField
+    @IBOutlet var nickelTextField : UITextField
+    @IBOutlet var dimeTextField : UITextField
+    @IBOutlet var quarterTextField : UITextField
+    @IBOutlet var fiftyCentTextField : UITextField
+    @IBOutlet var singleDollarTextField : UITextField
+    @IBOutlet var twoDollarTextField : UITextField
+    @IBOutlet var fiveDollarTextField : UITextField
+    @IBOutlet var tenDollarTextField : UITextField
+    @IBOutlet var twentyDollarTextField : UITextField
+    @IBOutlet var fiftyDollarTextField : UITextField
+    @IBOutlet var hundredDollarTextField : UITextField
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        pennyTextField.layer.borderColor = CGColorCreateGenericRGB(100, 50, 200, 1)
+        pennyTextField.layer.borderWidth = 0.5
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,13 +59,13 @@ class ViewController: UIViewController {
     @IBAction func pennyCounter(sender : UITextField) {
         str = pennyTextField.text.toInt()!
         
-        pennyCount = Float(str) * 0.01
+        pennyCount = Double(str) * 0.01
         
-        self.UpdateLabels()
+        UpdateLabels()
     }
     
     func UpdateLabels() {
-        pennyTotalLabel.text = String(str)
+        pennyTotalLabel.text = String(pennyCount)
         
     }
 
