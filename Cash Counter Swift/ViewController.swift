38e538f9d113ec30f758cbe3876f90f5d39d4deb
Dan Fairbanks <fairbanksdan@gmail.com>
2014-06-04 22:09:19 -0700

    Initial Commit

---
+++ Cash Counter Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cash Counter Swift
+//
+//  Created by Daniel Fairbanks on 6/4/14.
+//  Copyright (c) 2014 Fairbanksdan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
