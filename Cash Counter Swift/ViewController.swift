24b484600abd2f8311135e86e2f9fd872d9478a6
Dan Fairbanks <fairbanksdan@gmail.com>
2014-06-11 18:53:07 -0700

    Added gesture recognizer to resign the keyboard on tap

--- Cash Counter Swift/ViewController.swift
+++ Cash Counter Swift/ViewController.swift
@@ -7,11 +7,24 @@
 //
 
 import UIKit
+import QuartzCore
 
-class ViewController: UIViewController {
-    var str: Int = 0
-    var pennyCount: Double = 0.00
-    let pennymultiplier = 0.01
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
+    var str : Int?
+    var pennyCount = 0.00
+    var nickelCount = 0.00
+    var dimeCount = 0.00
+    var quarterCount = 0.00
+    var fiftyCentCount = 0.00
+    var singleDollarCount = 0.00
+    var twoDollarCount = 0.00
+    var fiveDollarCount = 0.00
+    var tenDollarCount = 0.00
+    var twentyDollarCount = 0.00
+    var fiftyDollarCount = 0.00
+    var hundredDollarCount = 0.00
+    
+    @IBOutlet var scrollView : UIScrollView
     
     @IBOutlet var resetButton : UIButton
     @IBOutlet var grandTotalLabel : UILabel
@@ -41,13 +54,52 @@ class ViewController: UIViewController {
     @IBOutlet var fiftyDollarTextField : UITextField
     @IBOutlet var hundredDollarTextField : UITextField
     
+    @IBOutlet var screenTapped : UITapGestureRecognizer = nil
+    
+    @IBAction func tapToCloseKeyboard(sender : UITapGestureRecognizer) {
+        
+        pennyTextField.resignFirstResponder()
+        nickelTextField.resignFirstResponder()
+        dimeTextField.resignFirstResponder()
+        quarterTextField.resignFirstResponder()
+        fiftyCentTextField.resignFirstResponder()
+        singleDollarTextField.resignFirstResponder()
+        twoDollarTextField.resignFirstResponder()
+        fiveDollarTextField.resignFirstResponder()
+        tenDollarTextField.resignFirstResponder()
+        twentyDollarTextField.resignFirstResponder()
+        fiftyDollarTextField.resignFirstResponder()
+        hundredDollarTextField.resignFirstResponder()
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        pennyTextField.layer.borderColor = CGColorCreateGenericRGB(100, 50, 200, 1)
         pennyTextField.layer.borderWidth = 0.5
+        pennyTextField.layer.borderColor = CGColorCreateGenericRGB(255, 255, 255, 1)
+        nickelTextField.layer.borderWidth = 0.5
+        nickelTextField.layer.borderColor = CGColorCreateGenericRGB(255, 255, 255, 1)
+        dimeTextField.layer.borderWidth = 0.5
+        dimeTextField.layer.borderColor = CGColorCreateGenericRGB(255, 255, 255, 1)
+        quarterTextField.layer.borderWidth = 0.5
+        quarterTextField.layer.borderColor = CGColorCreateGenericRGB(255, 255, 255, 1)
+        fiftyCentTextField.layer.borderWidth = 0.5
+        fiftyCentTextField.layer.borderColor = CGColorCreateGenericRGB(255, 255, 255, 1)
+        singleDollarTextField.layer.borderWidth = 0.5
+        singleDollarTextField.layer.borderColor = CGColorCreateGenericRGB(255, 255, 255, 1)
+        twoDollarTextField.layer.borderWidth = 0.5
+        twoDollarTextField.layer.borderColor = CGColorCreateGenericRGB(255, 255, 255, 1)
+        fiveDollarTextField.layer.borderWidth = 0.5
+        fiveDollarTextField.layer.borderColor = CGColorCreateGenericRGB(255, 255, 255, 1)
+        tenDollarTextField.layer.borderWidth = 0.5
+        tenDollarTextField.layer.borderColor = CGColorCreateGenericRGB(255, 255, 255, 1)
+        twentyDollarTextField.layer.borderWidth = 0.5
+        twentyDollarTextField.layer.borderColor = CGColorCreateGenericRGB(255, 255, 255, 1)
+        fiftyDollarTextField.layer.borderWidth = 0.5
+        fiftyDollarTextField.layer.borderColor = CGColorCreateGenericRGB(255, 255, 255, 1)
+        hundredDollarTextField.layer.borderWidth = 0.5
+        hundredDollarTextField.layer.borderColor = CGColorCreateGenericRGB(255, 255, 255, 1)
         
     }
 
@@ -57,18 +109,72 @@ class ViewController: UIViewController {
     }
     
     @IBAction func pennyCounter(sender : UITextField) {
-        str = pennyTextField.text.toInt()!
-        
-        pennyCount = Double(str) * 0.01
-        
+        if let str = pennyTextField.text?.toInt(){
+            pennyCount = Double(str) * 0.01
+        } else {
+            pennyCount = 0.00
+        }
+        UpdateLabels()
+    }
+
+    @IBAction func nickelCounter(sender : UITextField) {
+        if let str = nickelTextField.text?.toInt(){
+            nickelCount = Double(str) * 0.05
+        } else {
+            nickelCount = 0.00
+        }
+        UpdateLabels()
+    }
+    
+    @IBAction func dimeCounter(sender : UITextField) {
+        if let str = dimeTextField.text?.toInt(){
+            dimeCount = Double(str) * 0.10
+        } else {
+            dimeCount = 0.00
+        }
+        UpdateLabels()
+    }
+    
+    @IBAction func quarterCount(sender : UITextField) {
+        if let str = quarterTextField.text?.toInt(){
+            quarterCount = Double(str) * 0.25
+        } else {
+            quarterCount = 0.00
+        }
+        UpdateLabels()
+    }
+    
+    @IBAction func fiftyCentCounter(sender : UITextField) {
+        if let str = fiftyCentTextField.text?.toInt(){
+            fiftyCentCount = Double(str) * 0.50
+        } else {
+            fiftyCentCount = 0.00
+        }
+        UpdateLabels()
+    }
+    
+    @IBAction func singledollarCounter(sender : UITextField) {
+        if let str = singleDollarTextField.text?.toInt(){
+            singleDollarCount = Double(str) * 1.00
+        } else {
+            singleDollarCount = 0.00
+        }
         UpdateLabels()
     }
     
     func UpdateLabels() {
-        pennyTotalLabel.text = String(pennyCount)
+        grandTotalLabel.text = String(format:"%.2f", pennyCount + nickelCount + dimeCount + quarterCount + fiftyCentCount + singleDollarCount)
+        
+        pennyTotalLabel.text = String(format:"%.2f", pennyCount)
+        nickelTotalLabel.text = String(format:"%.2f", nickelCount)
+        dimeTotalLabel.text = String(format:"%.2f", dimeCount)
+        quarterTotalLabel.text = String(format:"%.2f", quarterCount)
+        fiftyCentTotalLabel.text = String(format:"%.2f", fiftyCentCount)
+        singleDollarTotalLabel.text = String(format:"%.2f", singleDollarCount)
         
     }
-
+    
+    
     @IBAction func ResetButtonPressed(sender : UIButton) {
     }
     
